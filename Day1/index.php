95d498034ee98cbef94319f1826e0dfedc129302
MumtazAysha <139667400+MumtazAysha@users.noreply.github.com>
2024-11-30 14:09:13 +0000

    Edit Students2

--- Day1/index.php
+++ Day1/index.php
@@ -9,9 +9,9 @@
         $result = mysqli_query($connect,$sql);
 
         //check if date exists
-        if(mysqli_num_rows( $result > 0)){
-            //fetch the data from rows
-            while($row = mysqli_fetch_assoc($result)){
+        if (mysqli_num_rows($result)>0) {
+            // fetch the data from rows
+            while ($row = mysqli_fetch_assoc($result)) {
                 print_r($row);
             }
         } else{
