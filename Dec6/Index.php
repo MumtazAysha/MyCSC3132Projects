f8e476f60eb3040376150a935256ccf8dd13d785
MumtazAysha <139667400+MumtazAysha@users.noreply.github.com>
2024-12-08 05:54:07 +0000

    e

--- Dec6/Index.php
+++ Dec6/Index.php
@@ -1,84 +1,18 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <style>
-        li:hover{
-            color:firebrick;
-            background-color: blanchedalmond;
-        }
-    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
 </head>
-<body></body>
-</html>
-<?php
-$num = array(1,array(2,4,7),64,7,array(2,3,array(2,5,7,8,array(4,6,7,8))),9,0);
-$productCategories = array(
-    'Electronics' => array(
-        'Smartphones' => array(
-            'Apple' => array(
-                'iPhone 13',
-                'iPhone 12 Pro',
-                'iPhone SE',
-            ),
-            'Samsung' => array(
-                'Galaxy S21',
-                'Galaxy Note 20',
-                'Galaxy A52',
-            ),
-        ),
-        'Laptops' => array(
-            'Dell' => array(
-                'XPS 13',
-                'Inspiron 15',
-            ),
-            'HP' => array(
-                'HP Spectre x360',
-                'HP Pavilion' => array(
-                    'Pavilion 15',
-                    'Pavilion x360',
-                    'Pavilion Gaming',
-                ),
-            ),
-        ),
-    ),
-    'Clothing' => array(
-        'Men' => array(
-            'T-Shirts',
-            'Jeans',
-            'Suits',
-        ),
-        'Women' => array(
-            'Dresses',
-            'Shoes',
-            'Handbags',
-        ),
-    ),
-    'Home & Kitchen' => array(
-        'Furniture' => array(
-            'Sofas',
-            'Tables',
-            'Chairs',
-        ),
-        'Appliances' => array(
-            'Refrigerators',
-            'Microwaves',
-            'Washing Machines',
-        ),
-    ),
-);
-function printArray($arr)
-{
-    foreach ($arr as $key => $item) {
-        if(is_array($item)){
-            echo "<ul>";
-            echo "<li>".$key."</li>";
-                echo "<ul>";
-                printArray($item);
-                echo "</ul>";
-            echo "</ul>";
-        }else{
-            echo "<li>".$item."</li>";
-        }
-    }
-}
+<body>
+    <h1>2020ASP67</h1><br><br>
 
-printArray($productCategories);
+    <a href="gettable.php">Gettable</a><br>
+    
+    <a href="myfun.php">MyFunction</a><br>
+
+
+    
+</body>
+</html>
